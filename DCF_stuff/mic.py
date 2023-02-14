08ca6553f5a650a109f582637d5bb1d4652a8d8d
DilanCF <dilancruzflores@gmail.com>
2023-02-14 12:46:19 -0500

    mic1 added

--- DCF_stuff/mic.py
+++ DCF_stuff/mic.py
@@ -3,7 +3,7 @@ import wave
 
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
-CHANNELS = 4
+CHANNELS = 1
 RATE = 44100
 RECORD_SECONDS = 5
 WAVE_OUTPUT_FILENAME = "output.wav"
---
+++ DCF_stuff/mic1.py
@@ -0,0 +1,43 @@
+# Source: https://fazals.ddns.net/spectrum-analyser-part-2/
+
+import numpy as np #importing Numpy with an alias np
+import pyaudio as pa 
+import struct 
+import matplotlib.pyplot as plt 
+
+CHUNK = 1024 * 1
+FORMAT = pa.paInt16
+CHANNELS = 1
+RATE = 44100 # in Hz
+
+p = pa.PyAudio()
+
+stream = p.open(
+    format = FORMAT,
+    channels = CHANNELS,
+    rate = RATE,
+    input=True,
+    output=True,
+    frames_per_buffer=CHUNK
+)
+
+
+
+fig, (ax,ax1) = plt.subplots(2)
+x_fft = np.linspace(0, RATE, CHUNK)
+x = np.arange(0,2*CHUNK,2)
+line, = ax.plot(x, np.random.rand(CHUNK),'r')
+line_fft, = ax1.semilogx(x_fft, np.random.rand(CHUNK), 'b')
+ax.set_ylim(-32000,32000)
+ax.ser_xlim = (0,CHUNK)
+ax1.set_xlim(20,RATE/2)
+ax1.set_ylim(0,1)
+fig.show()
+
+while 1:
+    data = stream.read(CHUNK)
+    dataInt = struct.unpack(str(CHUNK) + 'h', data)
+    line.set_ydata(dataInt)
+    line_fft.set_ydata(np.abs(np.fft.fft(dataInt))*2/(11000*CHUNK))
+    fig.canvas.draw()
+    fig.canvas.flush_events()
