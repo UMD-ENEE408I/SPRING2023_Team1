a223ea642a4d848d1bcbcefcf7732337ad28186b
DilanCF <dilancruzflores@gmail.com>
2023-03-14 13:47:12 -0400

    Added comments. Slight changes for simplicity

--- apriltag.py
+++ apriltag.py
@@ -3,6 +3,8 @@ import cv2
 import numpy as np
 import time
 
+# Questions for Levi:
+#       How does find_pose_from_tag work?
 
 at_detector = Detector(
     families="tag36h11",
@@ -25,9 +27,9 @@ laptop_K = "C:\\Users\\Dilan\\Documents\\GitHub\\SPRING2023_Team1\\DCF_stuff\\op
 laptop_D = "C:\\Users\\Dilan\\Documents\\GitHub\\SPRING2023_Team1\\DCF_stuff\\opencv\\cal\\op\\D.npy"
 
 # Get params
-DIM = np.load(jetson_DIM)
-K = np.load(jetson_K)
-D = np.load(jetson_D)
+DIM = np.load(laptop_DIM)
+K = np.load(laptop_K)
+D = np.load(laptop_D)
 
 balance = 0  # Set to 1 to show black space. Set to 0 to crop
 new_K = cv2.fisheye.estimateNewCameraMatrixForUndistortRectify(
@@ -37,6 +39,7 @@ map1, map2 = cv2.fisheye.initUndistortRectifyMap(
 
 
 def find_pose_from_tag(K, detection):
+
     m_half_size = tag_size / 2
 
     marker_center = np.array((0, 0, 0))
@@ -45,33 +48,46 @@ def find_pose_from_tag(K, detection):
     marker_points.append(marker_center + (m_half_size, m_half_size, 0))
     marker_points.append(marker_center + (m_half_size, -m_half_size, 0))
     marker_points.append(marker_center + (-m_half_size, -m_half_size, 0))
+    # Marker points is an array that contains the coords of the four corners on the tag.
+    # The last coord is always zero as the z dim, since its relative to the tag itself.
     _marker_points = np.array(marker_points)
 
     object_points = _marker_points
+    # The corners of the tag in image pixel coordinates. These always wrap counter-
+    # clock wise around the tag
     image_points = detection.corners
 
+    # pnp_ret -> [True | False, rotation vector, translation vector]
     pnp_ret = cv2.solvePnP(object_points, image_points,
                            K, distCoeffs=None, flags=cv2.SOLVEPNP_IPPE_SQUARE)
+
+    # First element tells us if the operation was successful or not
     if pnp_ret[0] == False:
         raise Exception('Error solving PnP')
 
+    # Rotation vector
     r = pnp_ret[1]
+    # Translation vector (Why is it labeled 'p'?)
     p = pnp_ret[2]
 
-    return p.reshape((3,)), r.reshape((3,))
+    # Before reshaping, p and r are arrays of arrays. This reduces them to just
+    # arrays whilst maintaining their values
+    p_fin = p.reshape((3,))
+    r_fin = r.reshape((3,))
+    return p_fin, r_fin
 
 
 if __name__ == '__main__':
     vid = cv2.VideoCapture(0)
 
-    tag_size = 0.16  # tag size in meters
+    tag_size = 0.13  # tag size in meters
 
     while True:
         try:
             ret, img = vid.read()
             # Undistorted image
             ud_img = cv2.remap(
-                    img, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
+                img, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
             gray = cv2.cvtColor(ud_img, cv2.COLOR_BGR2GRAY)
             gray.astype(np.uint8)
 
@@ -84,18 +100,31 @@ if __name__ == '__main__':
             results = at_detector.detect(gray, estimate_tag_pose=False)
 
             for res in results:
+                # print(res)
+                # Gets back both the rotation and translation matrices from solvePNP
                 pose = find_pose_from_tag(K, res)
+                # This will take in our translation VECTOR and turn it into a translation MATRIX.
+                # It will then set the value to itself, effectively overwritting the the vertex from before
+                # rot -> rotation matrix from previous
+                # jaco -> jacobian transform or nah (We don't use this)
                 rot, jaco = cv2.Rodrigues(pose[1], pose[1])
-                print(rot)
+                # print(rot)
 
                 pts = res.corners.reshape((-1, 1, 2)).astype(np.int32)
+                print("VVVV")
+                print("VVVV")
+                print("VVVV")
+                print(pts)
+                print("^^^")
+                print("^^^")
+                print("^^^")
                 ud_img = cv2.polylines(
                     ud_img, [pts], isClosed=True, color=(0, 0, 255), thickness=5)
                 cv2.circle(ud_img, tuple(res.center.astype(np.int32)),
                            5, (0, 0, 255), -1)
                 
 
-                print(pose)
+                # print(pose)
 
             cv2.imshow("img", ud_img)
             cv2.waitKey(10)
