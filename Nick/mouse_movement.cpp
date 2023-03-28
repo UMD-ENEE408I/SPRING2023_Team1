4d7eff211631bad03b2ce1fae8adf0fe367b57a4
nbooms <nicholasboomsma@gmail.com>
2023-03-28 12:09:46 -0400

    UDP Movement test

---
+++ Nick/mouse_movement.cpp
@@ -0,0 +1,371 @@
+#include <Arduino.h>
+#include <Adafruit_MCP3008.h>
+#include <Adafruit_MPU6050.h>
+#include <Encoder.h>
+
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <string.h>
+#include <iostream>
+
+// WiFi network name and password:
+const char * networkName = "408ITerps";
+const char * networkPswd = "goterps2022";
+char pbuff[255];
+
+// IP address to send UDP data to:
+// either use the ip address of the server or 
+// a network broadcast address
+const char * udpAddress = "192.168.2.102";
+const int udpPort = 3333; // Tracker 1: 3333, Tracker 2: 4444, Evader(?): 5555
+
+// Are we currently connected?
+boolean connected = false;
+
+// The udp library class
+WiFiUDP udp;
+
+// Wifi event handler
+void WiFiEvent(WiFiEvent_t event){
+    switch(event) {
+      case ARDUINO_EVENT_WIFI_STA_GOT_IP:
+          //When connected set 
+          Serial.print("WiFi connected! IP address: ");
+          Serial.println(WiFi.localIP());  
+          //initializes the UDP state
+          //This initializes the transfer buffer
+          udp.begin(WiFi.localIP(),udpPort);
+          connected = true;
+          break;
+      case ARDUINO_EVENT_WIFI_STA_DISCONNECTED:
+          Serial.println("WiFi lost connection");
+          connected = false;
+          break;
+      default: break;
+    }
+}
+
+void connectToWiFi(const char * ssid, const char * pwd){
+  Serial.println("Connecting to WiFi network: " + String(ssid));
+
+  // delete old config
+  WiFi.disconnect(true);
+  //register event handler
+  WiFi.onEvent(WiFiEvent);
+  
+  //Initiate connection
+  WiFi.begin(ssid, pwd);
+
+  Serial.println("Waiting for WIFI connection...");
+}
+
+// IMU (rotation rate and acceleration)
+Adafruit_MPU6050 mpu;
+Adafruit_MCP3008 adc1;
+Adafruit_MCP3008 adc2;
+
+// Buzzer pin which we will use for indicating IMU initialization failure
+const unsigned int BUZZ = 26;
+const unsigned int BUZZ_CHANNEL = 0;
+
+// Need these pins to turn off light bar ADC chips
+const unsigned int ADC_1_CS = 2;
+const unsigned int ADC_2_CS = 17;
+
+// Battery voltage measurement constants
+const unsigned int VCC_SENSE = 27;
+const float ADC_COUNTS_TO_VOLTS = (2.4 + 1.0) / 1.0 * 3.3 / 4095.0;
+
+// Motor encoder pins
+const unsigned int M1_ENC_A = 39;
+const unsigned int M1_ENC_B = 38;
+const unsigned int M2_ENC_A = 37;
+const unsigned int M2_ENC_B = 36;
+
+// Motor power pins
+const unsigned int M1_IN_1 = 13;
+const unsigned int M1_IN_2 = 12;
+const unsigned int M2_IN_1 = 25;
+const unsigned int M2_IN_2 = 14;
+
+// Motor PWM channels
+const unsigned int M1_IN_1_CHANNEL = 8;
+const unsigned int M1_IN_2_CHANNEL = 9;
+const unsigned int M2_IN_1_CHANNEL = 10;
+const unsigned int M2_IN_2_CHANNEL = 11;
+
+const int M_PWM_FREQ = 5000;
+const int M_PWM_BITS = 8;
+const unsigned int MAX_PWM_VALUE = 255; // Max PWM given 8 bit resolution
+
+float METERS_PER_TICK = (3.14159 * 0.031) / 360.0;
+float TURNING_RADIUS_METERS = 4.3 / 100.0; // Wheels are about 4.3 cm from pivot point
+
+void configure_imu() {
+  // Try to initialize!
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      ledcWriteNote(BUZZ_CHANNEL, NOTE_C, 4);
+      delay(500);
+      ledcWriteNote(BUZZ_CHANNEL, NOTE_G, 4);
+      delay(500);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  mpu.setGyroRange(MPU6050_RANGE_1000_DEG);
+  mpu.setFilterBandwidth(MPU6050_BAND_260_HZ);
+}
+
+void read_imu(float& w_z) {
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+  w_z = g.gyro.z;
+}
+
+void est_imu_bias(float& E_w_z, int N_samples) {
+  float E_w_z_acc = 0.0;
+  for (unsigned int i = 0; i < N_samples; i++) {
+    float w_z;
+    read_imu(w_z);
+    E_w_z_acc += w_z;
+    delay(5);
+  }
+  E_w_z = E_w_z_acc / N_samples;
+}
+
+void configure_motor_pins() {
+  ledcSetup(M1_IN_1_CHANNEL, M_PWM_FREQ, M_PWM_BITS);
+  ledcSetup(M1_IN_2_CHANNEL, M_PWM_FREQ, M_PWM_BITS);
+  ledcSetup(M2_IN_1_CHANNEL, M_PWM_FREQ, M_PWM_BITS);
+  ledcSetup(M2_IN_2_CHANNEL, M_PWM_FREQ, M_PWM_BITS);
+
+  ledcAttachPin(M1_IN_1, M1_IN_1_CHANNEL);
+  ledcAttachPin(M1_IN_2, M1_IN_2_CHANNEL);
+  ledcAttachPin(M2_IN_1, M2_IN_1_CHANNEL);
+  ledcAttachPin(M2_IN_2, M2_IN_2_CHANNEL);
+}
+
+// Positive means forward, negative means backwards
+void set_motors_pwm(float left_pwm, float right_pwm) {
+  if (isnan(left_pwm)) left_pwm = 0.0;
+  if (left_pwm  >  255.0) left_pwm  =  255.0;
+  if (left_pwm  < -255.0) left_pwm  = -255.0;
+  if (isnan(right_pwm)) right_pwm = 0.0;
+  if (right_pwm >  255.0) right_pwm =  255.0;
+  if (right_pwm < -255.0) right_pwm = -255.0;
+
+  if (left_pwm > 0) {
+    ledcWrite(M1_IN_1_CHANNEL, 0);
+    ledcWrite(M1_IN_2_CHANNEL, (uint32_t)(left_pwm));
+  } else {
+    ledcWrite(M1_IN_1_CHANNEL, (uint32_t)-left_pwm);
+    ledcWrite(M1_IN_2_CHANNEL, 0);
+  }
+
+  if (right_pwm > 0) {
+    ledcWrite(M2_IN_1_CHANNEL, 0);
+    ledcWrite(M2_IN_2_CHANNEL, (uint32_t)(right_pwm));
+  } else {
+    ledcWrite(M2_IN_1_CHANNEL, (uint32_t)-right_pwm);
+    ledcWrite(M2_IN_2_CHANNEL, 0);
+  }
+}
+
+float update_pid(float dt, float kp, float ki, float kd,
+                 float x_d, float x,
+                 float& int_e, float abs_int_e_max, // last_x and int_e are updated by this function
+                 float& last_x) {
+  // Calculate or update intermediates
+  float e = x_d - x; // Error
+
+  // Integrate error with anti-windup
+  int_e = int_e + e * dt;
+  if (int_e >  abs_int_e_max) int_e =  abs_int_e_max;
+  if (int_e < -abs_int_e_max) int_e = -abs_int_e_max;
+
+  // Take the "Derivative of the process variable" to avoid derivative spikes if setpoint makes step change
+  // with abuse of notation, call this de
+  float de = -(x - last_x) / dt;
+  last_x = x;
+
+  float u = kp * e + ki * int_e + kd * de;
+  return u;
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  // Stop the right motor by setting pin 14 low
+  // this pin floats high or is pulled
+  // high during the bootloader phase for some reason
+  pinMode(14, OUTPUT);
+  digitalWrite(14, LOW);
+  delay(100);
+
+  //Connect to the WiFi network
+  connectToWiFi(networkName, networkPswd);
+
+  //Send a packet
+  udp.beginPacket(udpAddress, udpPort);
+  udp.printf("Hi Jetson");
+  udp.endPacket();
+
+  // Disable the lightbar ADC chips so they don't hold the SPI bus used by the IMU
+  pinMode(ADC_1_CS, OUTPUT);
+  pinMode(ADC_2_CS, OUTPUT);
+  digitalWrite(ADC_1_CS, HIGH);
+  digitalWrite(ADC_2_CS, HIGH);
+
+  ledcAttachPin(BUZZ, BUZZ_CHANNEL);
+
+  pinMode(VCC_SENSE, INPUT);
+
+  configure_motor_pins();
+  configure_imu();
+
+  Serial.println("Starting!");
+}
+
+void loop() {
+  // Create the encoder objects after the motor has
+  // stopped, else some sort exception is triggered
+  Encoder enc1(M1_ENC_A, M1_ENC_B);
+  Encoder enc2(M2_ENC_A, M2_ENC_B);
+
+  // Loop period
+  int target_period_ms = 2; // Loop takes about 3 ms so a delay of 2 gives 200 Hz or 5ms
+
+  // Motors are controlled by a position PID
+  // with inputs interpreted in meters and outputs interpreted in volts
+  // integral term has "anti-windup"
+  // derivative term uses to derivative of process variable (wheel position)
+  // instead of derivative of error in order to avoid "derivative kick"
+  float kp_left = 200.0;
+  float ki_left = 20.0;
+  float kd_left = 20.0;
+  float kf_left = 10.0;
+  float target_pos_left  = 0.0;
+  float last_pos_left = 0.0;
+  float integral_error_pos_left = 0.0;
+  float max_integral_error_pos_left = 1.0 * 8.0 / ki_left; // Max effect is the nominal battery voltage
+
+  float kp_right = 200.0;
+  float ki_right = 20.0;
+  float kd_right = 20.0;
+  float kf_right = 10.0;
+  float last_pos_right = 0.0;
+  float target_pos_right = 0.0;
+  float integral_error_pos_right = 0.0;
+  float max_integral_error_pos_right = 1.0 * 8.0 / ki_right; // Max effect is the nominal battery voltage
+
+  // IMU Orientation variables
+  float theta = 0.0;
+  float bias_omega;
+  // Gain applied to heading error when offseting target motor velocities
+  // currently set to 360 deg/s compensation for 90 degrees of error
+  float ktheta = (2 * 3.14159) / (90.0 * 3.14159 / 180.0);
+  est_imu_bias(bias_omega, 500);// Could be expanded for more quantities
+
+  // Initializing cam_theta, target_theta and target_v
+  float cam_theta, target_theta, target_v;
+
+  // The real "loop()"
+  // time starts from 0
+  float start_t = (float)micros() / 1000000.0;
+  float last_t = -target_period_ms / 1000.0; // Offset by expected looptime to avoid divide by zero
+  while (true) {
+    // Get the time elapsed
+    float t = ((float)micros()) / 1000000.0 - start_t;
+    float dt = ((float)(t - last_t)); // Calculate time since last update
+    // Serial.print("t "); Serial.print(t);
+    Serial.print(" dt "); Serial.print(dt * 1000.0);
+    last_t = t;
+  
+    // TODO Battery voltage compensation, the voltage sense on my mouse is broken for some reason
+    // int counts = analogRead(VCC_SENSE);
+    // float battery_voltage = counts * ADC_COUNTS_TO_VOLTS;
+    // if (battery_voltage <= 0) Serial.println("BATTERY INVALID");
+
+    // Get the distances the wheels have traveled in meters
+    // positive is forward
+    float pos_left  =  (float)enc1.read() * METERS_PER_TICK;
+    float pos_right = -(float)enc2.read() * METERS_PER_TICK; // Take negative because right counts upwards when rotating backwards
+  
+    // Read IMU and update estimate of heading
+    // positive is counter clockwise
+    float omega;
+    read_imu(omega); // Could be expanded to read more things
+    omega -= bias_omega; // Remove the constant bias measured in the beginning
+    theta = theta + omega * dt;
+
+    // **EVERYTHING ABOVE IS UPDATING THE CURRENT "theta" VALUE FROM THE MOUSE'S PERSPECTIVE**
+    // Will have to check if the theta from the calculations/gyroscope is equal to that calculated from the camera/apriltags
+
+    //only send data when connected
+    if(connected){
+      //Send a packet
+      udp.beginPacket(udpAddress,udpPort);
+      udp.printf("Seconds since boot: %lu", millis()/1000);
+      udp.endPacket();
+    }
+    //Wait for 1 second
+    delay(100);
+
+    //Receive new data from Jetson
+    int packetSize = udp.parsePacket();
+    if(packetSize >= sizeof(float))
+    {
+      //Serial.printf("packet size is %d\n", packetSize);
+      float my_array[3]; 
+      udp.read((char*)my_array, sizeof(my_array)); 
+      udp.flush();
+      cam_theta = my_array[0];
+      target_theta = my_array[1];
+      target_v = my_array[2];
+    }
+
+    // **EVERYTHING BELOW IS REQUIRED TO CALCULATE THE NEXT DIRECTION/VELOCITY GIVEN "target_theta" AND "target_v"**
+  
+    // Calculate target motor speeds from target forward speed and target heading
+    // Could also include target path length traveled and target angular velocity
+    float error_theta_z = target_theta - theta;
+    float requested_v = target_v;
+    float requested_w = ktheta * error_theta_z;
+
+    float target_v_left  = requested_v - TURNING_RADIUS_METERS * requested_w;
+    float target_v_right = requested_v + TURNING_RADIUS_METERS * requested_w;
+    target_pos_left  = target_pos_left  + dt * target_v_left;
+    target_pos_right = target_pos_right + dt * target_v_right;
+
+    // Serial.print(" tpl "); Serial.print(target_pos_left);
+    // Serial.print(" pl "); Serial.print(pos_left);
+    // Serial.print(" tpr "); Serial.print(target_pos_right);
+    // Serial.print(" pr "); Serial.print(pos_right);
+
+    // Left motor position PID
+    float left_voltage = update_pid(dt, kp_left, ki_left, kd_left,
+                                    target_pos_left, pos_left,
+                                    integral_error_pos_left, max_integral_error_pos_left,
+                                    last_pos_left);
+    left_voltage = left_voltage + kf_left * target_v_left;
+    float left_pwm = (float)MAX_PWM_VALUE * (left_voltage / 8.0); // TODO use actual battery voltage
+
+    // Right motor position PID
+    float right_voltage = update_pid(dt, kp_right, ki_right, kd_right,
+                                     target_pos_right, pos_right,
+                                     integral_error_pos_right, max_integral_error_pos_right,
+                                     last_pos_right);
+    left_voltage = right_voltage + kf_right * target_v_right;
+    float right_pwm = (float)MAX_PWM_VALUE * (right_voltage / 8.0); // TODO use actual battery voltage
+
+    // Serial.print(" l voltage " ); Serial.print(left_voltage);
+    // Serial.print(" r voltage " ); Serial.print(right_voltage);
+
+    set_motors_pwm(left_pwm, right_pwm);
+
+    // Serial.println();
+    delay(target_period_ms);
+  }
+}
