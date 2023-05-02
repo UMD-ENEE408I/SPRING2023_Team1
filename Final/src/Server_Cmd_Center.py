3321d2d382a722122b23ff127e565f5674f63e21
nbooms <nicholasboomsma@gmail.com>
2023-05-02 13:04:32 -0400

    testing stuff

--- Final/src/Server_Cmd_Center.py
+++ Final/src/Server_Cmd_Center.py
@@ -102,7 +102,7 @@ def find_pose_from_tag(K, detection):
     r_fin = r.reshape((3,))
     return p_fin, r_fin
 
-"""
+
 localIP = ""
 
 localPort1 = 3333
@@ -110,7 +110,7 @@ localPort2 = 4444
 localPort3 = 5555
 
 bufferSize = 1024
-"""
+
 # Detector object
 at_detector = Detector(
     families="tag36h11",
@@ -121,18 +121,18 @@ at_detector = Detector(
     decode_sharpening=0.25,
     debug=0
 )
-"""
+
 # Create a datagram socket
 
 UDPServerSocket1 = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-UDPServerSocket2 = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-UDPServerSocket3 = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+#UDPServerSocket2 = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+#UDPServerSocket3 = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
 # Bind to address and ip
 
 UDPServerSocket1.bind((localIP, localPort1))
-UDPServerSocket2.bind((localIP, localPort2))
-UDPServerSocket3.bind((localIP, localPort3))
+#UDPServerSocket2.bind((localIP, localPort2))
+#UDPServerSocket3.bind((localIP, localPort3))
 
 print("UDP server up and ready to send a packet")
 
@@ -143,39 +143,39 @@ target_v = 0.2
 
 bytesAddressPair1 = UDPServerSocket1.recvfrom(bufferSize)
 print("Mouse 1 Address Received")
-bytesAddressPair2 = UDPServerSocket2.recvfrom(bufferSize)
+#bytesAddressPair2 = UDPServerSocket2.recvfrom(bufferSize)
 print("Mouse 2 Address Received")
-bytesAddressPair3 = UDPServerSocket3.recvfrom(bufferSize)
+#bytesAddressPair3 = UDPServerSocket3.recvfrom(bufferSize)
 print("Mouse 3 Address Received")
 
 
 message1 = bytesAddressPair1[0]
 address1 = bytesAddressPair1[1]
-message2 = bytesAddressPair2[0]
-address2 = bytesAddressPair2[1]
-message3 = bytesAddressPair3[0]
-address3 = bytesAddressPair3[1]
+#message2 = bytesAddressPair2[0]
+#address2 = bytesAddressPair2[1]
+#message3 = bytesAddressPair3[0]
+#address3 = bytesAddressPair3[1]
 
 
 clientMsg1 = "Message from Client1:{}".format(message1)
 clientIP1 = "Client1 IP Address:{}".format(address1)
-clientMsg2 = "Message from Client2:{}".format(message2)
-clientIP2 = "Client2 IP Address:{}".format(address2)
-clientMsg3 = "Message from Client3:{}".format(message3)
-clientIP3 = "Client3 IP Address:{}".format(address3)
+#clientMsg2 = "Message from Client2:{}".format(message2)
+#clientIP2 = "Client2 IP Address:{}".format(address2)
+#clientMsg3 = "Message from Client3:{}".format(message3)
+#clientIP3 = "Client3 IP Address:{}".format(address3)
 
 print(clientMsg1)
 print(clientIP1)
-print(clientMsg2)
-print(clientIP2)
-print(clientMsg3)
-print(clientIP3)
+#print(clientMsg2)
+#print(clientIP2)
+#print(clientMsg3)
+#print(clientIP3)
 
 # Give time for mice to setup
 time.sleep(5)
 
 # Listen for incoming datagrams
-"""
+
 while (True):
     # Get sound data
 
